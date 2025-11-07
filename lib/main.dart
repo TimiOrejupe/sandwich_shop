05101b4554fd42ba61e7944b4c5fe490f06cd999
TimiOrejupe <107513334+TimiOrejupe@users.noreply.github.com>
2025-11-07 14:20:41 +0000

    Implement dynamic text in OrderItemDisplay

--- lib/main.dart
+++ lib/main.dart
@@ -8,6 +8,20 @@ void main() {
 class App extends StatelessWidget {
   const App({super.key});
 
+// ignore: non_constant_identifier_names
+class OrderItemDisplay extends StatelessWidget {
+  final String itemType;
+  final int quantity;
+
+  const OrderItemDisplay(this.quantity, this.itemType, {super.key});
+
+  @override
+Widget build(BuildContext context) {
+  return Text('$quantity $itemType sandwich(es): ${'🥪' * quantity}');
+}
+  }
+}
+
   @override
 Widget build(BuildContext context) {
   return MaterialApp(
@@ -76,19 +90,8 @@ class _MyHomePageState extends State<MyHomePage> {
         // Center is a layout widget. It takes a single child and positions it
         // in the middle of the parent.
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
+   
+  
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             const Text('You have pushed the button this many times:'),
